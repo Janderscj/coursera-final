6917e24fec22088e3ca631b04302a9ac2f2d4c8b
Janderscj <jj.anderscj@gmail.com>
2026-02-23 14:06:13 -0700

    Installed React Router, Wrapped app in route in index.js

--- src/App.js
+++ src/App.js
@@ -7,13 +7,14 @@ import BookingPage from "./Components/Pages/BookingPage";
 
 function App() {
   return (
-    <div className="page-container">
+    <>
       <Nav />
+
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/reservations" element={<BookingPage />} />
       </Routes>
-    </div>
+    </>
   );
 }
 
