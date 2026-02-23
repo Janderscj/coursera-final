17b8d41c1c13c17fd8105dc911d7469ef8c89ebb
Janderscj <jj.anderscj@gmail.com>
2026-02-23 13:16:01 -0700

    Added Section imports to Main, Added jpg to Hero Section, Styled in CSS

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,11 +1,15 @@
 import HeroSection from "./Sections/HeroSection";
 import SpecialsSection from "./Sections/SpecialsSection";
+import HighlightsSection from "./Sections/Highlights";
+import TestimonialsSection from "./Sections/Testimonials";
 
 function Main() {
   return (
     <main>
       <HeroSection />
       <SpecialsSection />
+      <HighlightsSection />
+      <TestimonialsSection />
     </main>
   );
 }
--- src/Components/Sections/HeroSection.js
+++ src/Components/Sections/HeroSection.js
@@ -1,9 +1,15 @@
+import HeroImg from "../../Assets/restauranfood.jpg";
 function HeroSection() {
   return (
     <section className="hero-section">
-      <h1>Little Lemon</h1>
-      <h2>Chicago</h2>
-      <p>Fresh Mediterranean flavors.</p>
+      <div>
+        <h1>Little Lemon</h1>
+        <h2>Chicago</h2>
+        <p>Fresh Mediterranean flavors.</p>
+      </div>
+      <div className="hero-image-wrapper">
+        <img src={HeroImg} alt="Restaurant Food" className="hero-image" />
+      </div>
     </section>
   );
 }
