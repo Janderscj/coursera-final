3a15c99882c768d9f38563abc354af70740c94b0
Janderscj <jj.anderscj@gmail.com>
2026-02-23 14:25:03 -0700

    Updated Links in Nav to mirror figma wireframe, styled with css

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -9,8 +9,10 @@ function Nav() {
       <nav className="nav-links">
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
-        <Link to="/reservations">Reservations</Link>
         <Link to="/menu">Menu</Link>
+        <Link to="/reservations">Reservations</Link>
+        <Link to="/order">Order Online</Link>
+        <Link to="/login">Login</Link>
       </nav>
     </header>
   );
