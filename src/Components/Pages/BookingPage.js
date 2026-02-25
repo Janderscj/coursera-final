05b202ecc46aae1dd665b65aa1ce6da58959353d
Janderscj <jj.anderscj@gmail.com>
2026-02-25 08:41:40 -0700

    added booking form component, starter form code for booking form component and converted to jsx

--- src/Components/Pages/BookingPage.js
+++ src/Components/Pages/BookingPage.js
@@ -1,22 +1,12 @@
 import BookingHero from "../Sections/BookingHero";
+import BookingForm from "../Sections/BookingForm";
 
 function BookingPage() {
   return (
     <section className="booking" id="reservations">
       <BookingHero />
       <h2>Reserve a Table</h2>
-      <form>
-        <label>Date</label>
-        <input type="date" />
-
-        <label>Time</label>
-        <input type="time" />
-
-        <label>Guests</label>
-        <input type="number" min="1" max="10" />
-
-        <button type="submit">Book Now</button>
-      </form>
+      <BookingForm />
     </section>
   );
 }
