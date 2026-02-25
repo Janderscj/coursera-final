f19c124a14c75c751515c9a4eb39e2224efde31b
Janderscj <jj.anderscj@gmail.com>
2026-02-25 08:46:28 -0700

    Added state control to BookingForm

--- src/Components/Sections/BookingForm.js
+++ src/Components/Sections/BookingForm.js
@@ -1,27 +1,64 @@
-function bookingForm() {
+import { useState } from "react";
+
+function BookingForm() {
+  const [date, setDate] = useState("");
+  const [time, setTime] = useState("");
+  const [guests, setGuests] = useState(1);
+  const [occasion, setOccasion] = useState("");
+
+  const [availableTimes] = useState([
+    "17:00",
+    "18:00",
+    "19:00",
+    "20:00",
+    "21:00",
+    "22:00",
+  ]);
+
   return (
     <section className="booking-form">
       <form>
         <label htmlFor="res-date">Choose date</label>
-        <input type="date" id="res-date" />
+        <input
+          type="date"
+          id="res-date"
+          value={date}
+          onChange={(e) => setDate(e.target.value)}
+        />
 
         <label htmlFor="res-time">Choose time</label>
-        <select id="res-time">
-          <option>17:00</option>
-          <option>18:00</option>
-          <option>19:00</option>
-          <option>20:00</option>
-          <option>21:00</option>
-          <option>22:00</option>
+        <select
+          id="res-time"
+          value={time}
+          onChange={(e) => setTime(e.target.value)}
+        >
+          {availableTimes.map((t) => (
+            <option key={t} value={t}>
+              {t}
+            </option>
+          ))}
         </select>
 
         <label htmlFor="guests">Number of guests</label>
-        <input type="number" placeholder="1" min="1" max="10" id="guests" />
+        <input
+          type="number"
+          id="guests"
+          placeholder="1"
+          min="1"
+          max="10"
+          value={guests}
+          onChange={(e) => setGuests(e.target.value)}
+        />
 
         <label htmlFor="occasion">Occasion</label>
-        <select id="occasion">
-          <option>Birthday</option>
-          <option>Anniversary</option>
+        <select
+          id="occasion"
+          value={occasion}
+          onChange={(e) => setOccasion(e.target.value)}
+        >
+          <option value="">Select an option</option>
+          <option value="Birthday">Birthday</option>
+          <option value="Anniversary">Anniversary</option>
         </select>
 
         <input type="submit" value="Make Your reservation" />
@@ -29,4 +66,5 @@ function bookingForm() {
     </section>
   );
 }
-export default bookingForm;
+
+export default BookingForm;
