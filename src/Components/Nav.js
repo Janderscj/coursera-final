0ad716c6948037c06bde78255e6720ad9b989030
Janderscj <jj.anderscj@gmail.com>
2026-03-08 17:05:05 -0600

    Added Hamburger menu to nav bar for mobile. Added css for hamburger menu.

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,9 +1,11 @@
 import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useState } from "react";
 import logo from "../Assets/Logo .svg";
 
 function Nav() {
   const location = useLocation();
   const navigate = useNavigate();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   function handleHomeClick(e) {
     if (location.pathname === "/") {
@@ -13,6 +15,7 @@ function Nav() {
       e.preventDefault();
       navigate("/");
     }
+    setIsMenuOpen(false);
   }
 
   //click handler for navigating
@@ -32,25 +35,42 @@ function Nav() {
         });
       }, 100);
     }
+    setIsMenuOpen(false);
   }
 
   return (
     <header className="top-header">
       <img src={logo} alt="Little Lemon Logo" className="logo" />
 
-      <nav className="nav-links" id="navbar">
+      {/* Hamburger button */}
+      <button
+        className="hamburger"
+        onClick={() => setIsMenuOpen(!isMenuOpen)}
+        aria-label="Toggle navigation"
+      >
+        ☰
+      </button>
+
+      {/* Nav links */}
+      <nav className={`nav-links ${isMenuOpen ? "open" : ""}`} id="navbar">
         <Link to="/" onClick={handleHomeClick}>
           Home
         </Link>
-
         <Link to="/" onClick={handleAboutClick}>
           About
         </Link>
-
-        <Link to="/menu">Menu</Link>
-        <Link to="/booking">Reservations</Link>
-        <Link to="/order">Order Online</Link>
-        <Link to="/login">Login</Link>
+        <Link to="/menu" onClick={() => setIsMenuOpen(false)}>
+          Menu
+        </Link>
+        <Link to="/booking" onClick={() => setIsMenuOpen(false)}>
+          Reservations
+        </Link>
+        <Link to="/order" onClick={() => setIsMenuOpen(false)}>
+          Order Online
+        </Link>
+        <Link to="/login" onClick={() => setIsMenuOpen(false)}>
+          Login
+        </Link>
       </nav>
     </header>
   );
