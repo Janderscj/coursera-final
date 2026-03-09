ebb40de98678f8c18dedd4bcf0f7249d9bea38e8
Janderscj <jj.anderscj@gmail.com>
2026-03-09 10:28:03 -0600

    Added 404.html from MIT to simulate application routing. Added scroll to top component to scroll to top of page when navigating to new page.

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import "./Styles/index.css";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import ScrollToTop from "./Components/ScrollToTop";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
+    <ScrollToTop />
     <App />
   </BrowserRouter>,
 );
